a6e36a92d52cd154933a878b996ebe7988264c6a
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-05-20 13:50:11 -0500

    Move set_word_pairs method to module

---
+++ app/models/concerns/word_pair_helper.rb
@@ -0,0 +1,17 @@
+module WordPairHelper
+  def set_word_pairs
+    # Remove new lines
+    single_line = contents.gsub(/\R+/, ' ')
+
+    # Standardize text
+    book_text = single_line.downcase.gsub(/[^0-9a-z' ]/, '')
+    book_array = book_text.split(' ')
+
+    # Set pairs and save to record
+    book_pairs = []
+    book_array.each_cons(2) do |pair|
+      book_pairs << pair 
+    end
+    self.word_pairs = book_pairs
+  end
+end
