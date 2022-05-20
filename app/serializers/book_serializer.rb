b5f1c79a0d45f17b575db0c057ed5ade84074b3a
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-05-20 15:06:21 -0500

    Add rough draft serializer

---
+++ app/serializers/book_serializer.rb
@@ -0,0 +1,20 @@
+class BookSerializer
+  def self.pairing_total_count(words, count)
+    {
+    "data": {
+        "id": "null",
+        "type": "book",
+        "attributes": {
+              "word_pairing": {
+                                  "word_pairing": words
+                                  "total_count": count
+                                  }
+                                }
+                              }
+                            }
+
+
+  end
+  
+
+end
