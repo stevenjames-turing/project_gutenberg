61ffb6a8ce971bcb54174ddad902eb981cb4c7d0
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-05-20 15:03:41 -0500

    word_pairing_count returns count of word pairing passed

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,7 +4,7 @@ class Book < ApplicationRecord
   after_create :create_chapters
   serialize :word_pairs
   
-  has_many :chapters
+  has_many :chapters, dependent: :destroy
   
   def create_chapters 
     chapter_contents = contents.split(/\r\n\r\n\r\n\r\n\r\nChapter/)
@@ -13,5 +13,9 @@ class Book < ApplicationRecord
     chapter_contents[1..-1].each do |chapter_content|
       Chapter.create!(contents: chapter_content, book_id: id)
     end
+  end
+
+  def word_pairing_count(words)
+    word_pairs[[words[0], words[1]]]
   end
 end
