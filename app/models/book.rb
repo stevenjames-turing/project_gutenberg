d4980b730d6fc2431dad06240e21644d2761967e
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-05-20 13:22:11 -0500

    Feat: book sets word_pairs upon creation

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,15 @@
 class Book < ApplicationRecord
-  validates_presence_of :contents, :word_pairs
+  before_validation :set_word_pairs
+
+  serialize :word_pairs
+
+  def set_word_pairs
+    book_text = contents.downcase.gsub(/[^0-9a-z' ]/, '')
+    book_array = book_text.split(' ')
+    book_pairs = []
+    book_array.each_cons(2) do |pair|
+      book_pairs << pair 
+    end
+    self.word_pairs = book_pairs
+  end
 end
