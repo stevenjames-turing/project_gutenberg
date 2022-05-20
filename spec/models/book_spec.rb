a93cad212f049b49f46ec52508581654ec6014a9
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-05-20 13:21:43 -0500

    Test: book sets word_pairs upon creation

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,8 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  describe 'validations' do
-    it { should validate_presence_of :contents }
-    it { should validate_presence_of :word_pairs }
+  
+  # describe 'validations' do
+  #   it { should validate_presence_of :contents }
+  #   it { should validate_presence_of :word_pairs }
+  # end
+
+  describe 'book creation', :vcr do 
+    it 'sets word pairs using full book contents' do 
+      uri = URI('https://www.gutenberg.org/files/1400/1400-0.txt')
+      response = Net::HTTP.get_response(uri)
+
+      book = Book.create!(contents: response.body)
+
+      expect(book.word_pairs).to_not be nil 
+
+      book.word_pairs.each do |pair|
+        expect(pair.count).to eq(2)
+      end
+    end
   end
 end
